82b958139f419d59f0d4df3a83b6b59b677c082f
Ilay Shefer <ilay90411@gmail.com>
2026-01-26 01:58:45 +0200

    feat:add swagger annontaion for comment route

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -4,16 +4,163 @@ import { authenticate } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /comments:
+ *   post:
+ *     summary: Add a new comment
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - postId
+ *               - content
+ *             properties:
+ *               postId:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Comment created
+ *       400:
+ *         description: Invalid input
+ *       401:
+ *         description: Unauthorized
+ */
 router.post("/", authenticate, commentController.addComment);
 
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Get all comments
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: postId
+ *         schema:
+ *           type: string
+ *         description: Get all comments
+ *     responses:
+ *       200:
+ *         description: List of comments
+ *       401:
+ *         description: Unauthorized
+ */
 router.get("/", authenticate, commentController.getAllComments);
 
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Get all comments by post
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: postId
+ *         schema:
+ *           type: string
+ *         description: Filter comments by post ID
+ *     responses:
+ *       200:
+ *         description: List of post comments
+ *       400:
+ *         description: Missing postId
+ *       401:
+ *         description: Unauthorized
+ */
 router.get("/post", authenticate, commentController.getCommentsByPost);
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   get:
+ *     summary: Get a comment by ID
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Comment ID
+ *     responses:
+ *       200:
+ *         description: Comment retrieved
+ *       404:
+ *         description: Comment not found
+ */
 router.get("/:id", authenticate, commentController.getCommentById);
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   put:
+ *     summary: Update a comment
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Comment ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Comment updated
+ *       400:
+ *         description: Invalid content
+ *       403:
+ *         description: Forbidden
+ */
 router.put("/:id", authenticate, commentController.updateComment);
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   delete:
+ *     summary: Delete a comment
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Comment ID
+ *     responses:
+ *       200:
+ *         description: Comment deleted
+ *       403:
+ *         description: Forbidden
+ */
 router.delete("/:id", authenticate, commentController.deleteComment);
 
 export default router;
