a4f1a6a1985e0df756d46b2b48530290f29f78cb
yahav <yahavminikes@gmail.com>
2026-01-26 01:17:57 +0200

    add auth route and controller

---
+++ src/routes/authRoute.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { authenticate } from "../../middlewares/authMiddleware";
+import authController from "../controllers/authController";
+
+const router = express.Router();
+
+
+router.post("/register", authController.register);
+
+router.post("/refresh", authenticate, authController.refreshToken);
+
+router.post('/logout', authenticate, authController.logout);
+
+export default router;
