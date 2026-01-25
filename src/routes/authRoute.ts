9112fcdb3745b36de47dd38044005e7efd1cd3d4
yahav <yahavminikes@gmail.com>
2026-01-26 01:18:55 +0200

    add login route

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.post("/register", authController.register);
 
+router.post("/login", authController.login);
+
 router.post("/refresh", authenticate, authController.refreshToken);
 
 router.post('/logout', authenticate, authController.logout);
